1b663395d1ed89256011cab03a90e66ac015652a
aratislamsafol <aratislamsafol@gmail.com>
2022-06-25 00:22:35 +0600

    Top Nav, navbar Complete

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+  $('#fun').click(function(){
+  $('#set').slideToggle(400);
+  });
+});
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "68%";
+  document.getElementById("main").style.marginLeft = "0px";
+  }
+
+  function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+  }
+
+
+
+
